397aa235e231909448bb633e4f0edddcf478ccb2
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2020-05-11 23:56:00 +0200

    snapshot

--- mathjax/index.js
+++ mathjax/index.js
@@ -1,12 +1,14 @@
 import m from "mithril";
 import {Math as M} from "./math.js";
 
+// OK, I guess that mithril cannot see this state change ?
 let content = "\\frac{1}{2} \\int"
 
 function main() {
   M.install();
   let body = document.getElementById("body");
   m.mount(body, {"view": () => [
+    m("p", content),
     m("p", "text before ", m(M, {content}), " text after."),
     m("button", {onclick: () => {
       console.log("content:", content);
--- mathjax/math.js
+++ mathjax/math.js
@@ -17,15 +17,21 @@ export class Math {
   }
 
   oncreate(vnode) {
-    return this.onupdate(vnode);
+    //this.onupdate(vnode);
   }
 
   onupdate(vnode) {
     if (Math.ready) {
       console.log("dom:", vnode.dom);
       console.log("HTML:", vnode.dom.innerHTML);
-      // MathJax.typeset(vnode.dom); // not iterable ? WTF ?
-      MathJax.typeset(); // doesn't work after the first time. Why ?
+      console.log(MathJax.typeset);
+      MathJax.typeset([vnode.dom]); // doesn't work after the first time. Why ?
+      // note that this elt is the PROCESSED mathjax node, the result, so that
+      // makes sense that mathjax doesn't reprocessed. Why has mithril not
+      // replaced this node by a new string ?
+
+      // try to replace this dom stuff manually (instead of typeset.Mathjax) 
+      // to see if the same issue happens.
       console.log("*");
     }
   }
@@ -33,6 +39,7 @@ export class Math {
   view(vnode) {
     let {attrs, children} = vnode;
     let {display = "inline", content} = attrs;
+    console.log("view() content:", content);
     if (display === "inline") {
         return m("span", "\\(" + content + "\\)");      
     }

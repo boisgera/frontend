ee37f8255b4789de77a7cda4053ed86862fe922e
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2020-05-12 23:05:42 +0200

    snapshot

--- mathjax/index.js
+++ mathjax/index.js
@@ -1,7 +1,7 @@
 import m from "mithril";
+let builtins = {Math};
 import {Math as M} from "./math.js";
 
-// OK, I guess that mithril cannot see this state change ?
 let content = "\\frac{1}{2} \\int"
 
 function main() {
@@ -9,7 +9,8 @@ function main() {
   let body = document.getElementById("body");
   m.mount(body, {"view": () => [
     m("p", content),
-    m("p", "text before ", [m(M, {key: content, content})], " text after."),
+    m("p", "text before ", m(M, {content}), " text after."),
+    m("p", "text before ", m(M, {display: "block", content}), " text after."),
     m("button", {onclick: () => {
       console.log("content:", content);
       content = content + " \\blacksquare";}
--- mathjax/math.js
+++ mathjax/math.js
@@ -1,12 +1,14 @@
 import m from "mithril";
 
 export class Math {
-
   static install() {
     window.MathJax = {
       startup: {
         pageReady: () => {
-          Math.ready = true;
+          Math.prototype.onupdate = function(vnode) {
+            MathJax.typeset([vnode.dom]); 
+          }
+          m.redraw(); 
         }
       }
     };
@@ -21,38 +23,18 @@ export class Math {
   }
 
   onupdate(vnode) {
-    if (Math.ready) {
-      console.log("dom:", vnode.dom);
-      console.log("HTML:", vnode.dom.innerHTML);
-      console.log(MathJax.typeset);
-      MathJax.typeset(); //([vnode.dom]); // doesn't work after the first time. Why ?
-      // note that this elt is the PROCESSED mathjax node, the result, so that
-      // makes sense that mathjax doesn't reprocessed. Why has mithril not
-      // replaced this node by a new string ?
-
-      // try to replace this dom stuff manually (instead of typeset.Mathjax) 
-      // to see if the same issue happens. 
-
-      // TODO; try to insert a changing key to force node recreation ?
-      // IT WORKS !!! ... but hackish so far. Have look at the doc for
-      // key in vue.js and i shall totally index the key on the content.
-      // MUCH better now.
-
-      // Otherwise, render once offline and embed into a m.trust ?
-      console.log("*");
-    }
   }
 
   view(vnode) {
-    let {attrs, children} = vnode;
+    let {attrs} = vnode;
     let {display = "inline", content} = attrs;
-    console.log("view() content:", content);
     if (display === "inline") {
-        return m("span", "\\(" + content + "\\)");      
+      content = `\\(${content}\\)`
+    } else { // block
+      content = `\\[${content}\\]`
     }
+    return [m("span", {key: content}, content)];      
   }
 }
-
-Math.ready = false;
 
 export default {Math};

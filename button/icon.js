84c3f540cdf485564dd719889a36521db67f82a5
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2020-05-19 23:00:33 +0200

    snapshot

--- button/icon.js
+++ button/icon.js
@@ -3,7 +3,8 @@ import m from "mithril";
 import feather from "feather-icons";
 import utils from "./utils.js"
 
-// TODO : Would need to generate once and for all all Icons as classes.
+// TODO : Would need to generate once and for all all Icons as classes
+//        and remove "name" from the attributes.
 //        We should NOT make a function that regenerate a new class
 //        since that would fuck up the way mithril works.
 //        Unless we cache the class that is returned that is ;
@@ -11,14 +12,15 @@ import utils from "./utils.js"
 //        Except merely wrt "style" issues.
 
 export class Icon {
-  view({attrs}) {
-    let {name, ...otherAttrs} = attrs;
-    console.log("name:", name)
+  view(vnode) {
+    let {attrs: {name, ...otherAttrs}} = vnode; 
     let icon = feather.icons[name].toSvg(otherAttrs);
       return m("span", {class: "icon"}, m.trust(icon));
   }
 }
 
+// The margin-right should probably not be here ; the container should
+// decide what the proper margin is.
 const sheet = j2c.sheet({
   "@global": {
     ".icon" : {

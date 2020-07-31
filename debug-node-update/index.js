0ea4894f59e25e3533fa74a484dbbab9be1f3c42
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2020-07-31 10:59:00 +0200

    snap

---
+++ debug-node-update/index.js
@@ -0,0 +1,29 @@
+import m from "mithril"
+
+let text = "Hello world!";
+
+class P {
+  view() {
+    console.log("P.view");
+    return m("p", text)
+  }
+  oncreate(vnode) {
+    console.log("P.oncreate");
+  }
+  onupdate(vnode) {
+    console.log("P.onupdate");
+    console.log(vnode.dom);
+    vnode.dom.innerHTML = "<p>" + Date.now() + "<p>" + vnode.dom.innerHTML;
+  }
+}
+
+class App {
+  view() {
+    return [
+      m(P),
+      m("button", {onclick: () => text += "Z"}, "Append Z")
+    ]    
+  }
+}
+
+m.mount(document.body, App)
